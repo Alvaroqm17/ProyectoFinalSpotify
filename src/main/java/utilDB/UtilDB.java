59c074f0631e1caaabe9fd5a74012f8c02365333
Alvaroqm17 <quinonesmeleroalvaro@gmail.com>
2022-05-30 13:35:00 +0200

    Conexion con la base de datos
    he creado la conexion con la base de datos en una clase que guarda los datos de la bdd en variables haciendo asi mas facil el cambio de bdd

---
+++ src/main/java/utilDB/UtilDB.java
@@ -0,0 +1,42 @@
+package utilDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public abstract class UtilDB {
+
+	private static final String cadenaConexion = "jdbc:mysql://127.0.0.1:3306/examen13t";
+	private static final String usuarioBD = "root";
+	private static final String contraseñaBD = "QMalvaro17";
+	private static Connection conexion;
+	
+	public static Statement conectarDB() {
+		
+	try {
+		if(conexion==null) {
+			conexion=DriverManager.getConnection(cadenaConexion,usuarioBD,contraseñaBD);
+			
+		}
+		return conexion.createStatement();
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.err.println("No se ha podido conectar intentelo luego");
+			return null;
+		}
+	}
+	
+	public static void desconectarBD() {
+		if(conexion!=null) {
+			try {
+				conexion.close();
+				conexion = null;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.err.println("No se pudo desconectar");
+			}
+		}
+	}
+}
