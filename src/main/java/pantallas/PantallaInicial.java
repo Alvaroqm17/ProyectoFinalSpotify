e99441e9ee6ad7bf38407e35504517fd734539d2
Alvaroqm17 <quinonesmeleroalvaro@gmail.com>
2022-06-10 15:07:18 +0200

    Solucion de bugs
    el pausa no funcionaba correctamente y he implementado un reproductor y una pausa a cada cancion de moey

--- src/main/java/pantallas/PantallaInicial.java
+++ src/main/java/pantallas/PantallaInicial.java
@@ -4,9 +4,6 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JScrollPane;
 import elemntosvisuales.BotonLista;
-import elemntosvisuales.ElementoMusica;
-import interfaz.ImplementarSonido;
-
 import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.Font;
@@ -16,24 +13,22 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.swing.BoxLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
-import java.awt.FlowLayout;
-import javax.swing.JLayeredPane;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.ImageIcon;
+import java.awt.FlowLayout;
 
-public class PantallaInicial extends JPanel  {
+public class PantallaInicial extends JPanel {
 	private PantallaActual ventana;
 	private AudioInputStream audioInputStream;
-	private Clip clip;
+	private Clip clipMoey1;
+	private Clip clipMoey2;
+	private Clip clipMoey3;
 
 	public PantallaInicial(PantallaActual v) {
 		this.ventana = v;
@@ -99,60 +94,104 @@ public class PantallaInicial extends JPanel  {
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(Color.BLACK);
 		scrollPane.setColumnHeaderView(panel_1);
+		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 
 		JLabel lblNewLabel_1 = new JLabel("Artista destacado!");
 		lblNewLabel_1.setBackground(Color.BLACK);
 		lblNewLabel_1.setForeground(Color.WHITE);
-		lblNewLabel_1.setFont(new Font("Cascadia Mono", Font.PLAIN, 14));
+		lblNewLabel_1.setFont(new Font("Cascadia Mono", Font.PLAIN, 17));
 		panel_1.add(lblNewLabel_1);
 
 		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.BLACK);
 		scrollPane.setViewportView(panel_2);
-		panel_2.setLayout(new BorderLayout(0, 0));
+		panel_2.setLayout(null);
 
-		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(Color.BLACK);
-		panel_2.add(panel_3, BorderLayout.NORTH);
+		JLabel letrasMoey = new JLabel("MOEY");
+		letrasMoey.setBounds(317, 30, 41, 23);
+		letrasMoey.setFont(new Font("MV Boli", Font.BOLD, 14));
+		letrasMoey.setForeground(Color.WHITE);
+		panel_2.add(letrasMoey);
+		irALibreria.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				ventana.cambiarAPantalla("libreria");
+			}
+		});
 
-		JPanel panel_4 = new JPanel();
-		panel_4.setBackground(Color.BLACK);
-		panel_2.add(panel_4, BorderLayout.CENTER);
-		GridBagLayout gbl_panel_4 = new GridBagLayout();
-		gbl_panel_4.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0, 0 };
-		gbl_panel_4.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		gbl_panel_4.columnWeights = new double[] { 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE };
-		gbl_panel_4.rowWeights = new double[] { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-				1.0, 0.0, Double.MIN_VALUE };
-		panel_4.setLayout(gbl_panel_4);
+		JButton moey1Pausa = new JButton("||");
+		moey1Pausa.setBounds(17, 86, 41, 23);
+		panel_2.add(moey1Pausa);
+		moey1Pausa.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
 
-		JLabel lblNewLabel_2 = new JLabel("MOEY");
-		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_2.gridx = 4;
-		gbc_lblNewLabel_2.gridy = 0;
-		panel_4.add(lblNewLabel_2, gbc_lblNewLabel_2);
-		lblNewLabel_2.setBackground(Color.BLACK);
-		lblNewLabel_2.setFont(new Font("Rockwell Extra Bold", Font.BOLD, 12));
-		lblNewLabel_2.setForeground(Color.WHITE);
+				clipMoey1.stop();
+				System.out.println("aa");
+			}
 
-		JLabel lblNewLabel_6 = new JLabel("");
-		lblNewLabel_6.setBackground(Color.BLACK);
-		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
-		gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_6.gridx = 4;
-		gbc_lblNewLabel_6.gridy = 1;
-		panel_4.add(lblNewLabel_6, gbc_lblNewLabel_6);
+		});
 
-		JButton moey1 = new JButton("\u25B6");
-		moey1.addMouseListener(new MouseAdapter() {
+		JButton moey2Pausa = new JButton("||");
+		moey2Pausa.setBounds(252, 86, 41, 23);
+		panel_2.add(moey2Pausa);
+		moey2Pausa.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				clipMoey2.stop();
+				System.out.println("aa");
+			}
+
+		});
+
+		JButton moey3Pausa = new JButton("||");
+		moey3Pausa.setBounds(491, 86, 41, 23);
+		panel_2.add(moey3Pausa);
+		moey3Pausa.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				clipMoey3.stop();
+				System.out.println("aa");
+			}
+
+		});
+
+		JLabel lblNewLabel_4 = new JLabel("");
+		lblNewLabel_4.setBounds(68, 86, 65, 65);
+		lblNewLabel_4.setIcon(new ImageIcon(
+				"D:\\Programacion\\ProyectoFinalSpotify\\ProyectoFinalSpotify\\imagenes\\cancionagustin.jpg"));
+		lblNewLabel_4.setForeground(Color.WHITE);
+		panel_2.add(lblNewLabel_4);
+
+		JLabel lblNewLabel_6 = new JLabel("\r\n");
+		lblNewLabel_6.setBounds(303, 86, 69, 64);
+		lblNewLabel_6.setIcon(
+				new ImageIcon("D:\\Programacion\\ProyectoFinalSpotify\\ProyectoFinalSpotify\\imagenes\\monomoey.jpeg"));
+		lblNewLabel_6.setForeground(Color.WHITE);
+		panel_2.add(lblNewLabel_6);
+
+		JLabel lblNewLabel_5 = new JLabel("\r\n");
+		lblNewLabel_5.setBounds(542, 86, 69, 65);
+		lblNewLabel_5.setIcon(new ImageIcon(
+				"D:\\Programacion\\ProyectoFinalSpotify\\ProyectoFinalSpotify\\imagenes\\mu\u00F1ecomoey.jpeg"));
+		lblNewLabel_5.setForeground(Color.WHITE);
+		panel_2.add(lblNewLabel_5);
+
+		JButton moey1Play = new JButton("\u25B6\uFE0E");
+		moey1Play.setBounds(10, 129, 53, 23);
+		panel_2.add(moey1Play);
+		moey1Play.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				try {
 					audioInputStream = AudioSystem.getAudioInputStream(new File("./sonidos/backcar.wav"));
-					clip = AudioSystem.getClip();
-					clip.open(audioInputStream);
-					clip.start();
+					clipMoey1 = AudioSystem.getClip();
+					clipMoey1.open(audioInputStream);
+					clipMoey1.start();
 					System.out.println("aa");
+
 				} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -162,132 +201,96 @@ public class PantallaInicial extends JPanel  {
 
 		});
 
-		JButton botonpausa = new JButton("||");
-		GridBagConstraints gbc_botonpausa = new GridBagConstraints();
-		gbc_botonpausa.insets = new Insets(0, 0, 5, 5);
-		gbc_botonpausa.gridx = 1;
-		gbc_botonpausa.gridy = 5;
-		panel_4.add(botonpausa, gbc_botonpausa);
-		botonpausa.addMouseListener(new MouseAdapter() {
+		JButton moey2Play = new JButton("\u25B6\uFE0E");
+		moey2Play.setBounds(240, 129, 53, 23);
+		panel_2.add(moey2Play);
+		moey2Play.addMouseListener(new MouseAdapter() {
 			@Override
-		public void mouseClicked(MouseEvent e) {
+			public void mouseClicked(MouseEvent e) {
+				try {
+					audioInputStream = AudioSystem.getAudioInputStream(new File("./sonidos/nukisashi.wav"));
+					clipMoey2 = AudioSystem.getClip();
+					clipMoey2.open(audioInputStream);
+					clipMoey2.start();
+				} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 
-				clip.stop();
-				System.out.println("aa");
 			}
 
 		});
 
-		GridBagConstraints gbc_moey1 = new GridBagConstraints();
-		gbc_moey1.insets = new Insets(0, 0, 5, 5);
-		gbc_moey1.gridx = 1;
-		gbc_moey1.gridy = 3;
-		panel_4.add(moey1, gbc_moey1);
+		JButton moey3Play = new JButton("\u25B6\uFE0E");
+		moey3Play.setBounds(479, 129, 53, 23);
+		panel_2.add(moey3Play);
+		moey3Play.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+					audioInputStream = AudioSystem.getAudioInputStream(new File("./sonidos/inmuneme.wav"));
+					clipMoey3 = AudioSystem.getClip();
+					clipMoey3.open(audioInputStream);
+					clipMoey3.start();
+				} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 
-		JLabel lblNewLabel_3 = new JLabel("");
-		lblNewLabel_3.setIcon(new ImageIcon(
-				"E:\\Programacion\\ProyectoFinalSpotify\\ProyectoFinalSpotify\\imagenes\\cancionagustin.jpg"));
-		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
-		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_3.gridx = 2;
-		gbc_lblNewLabel_3.gridy = 4;
-		panel_4.add(lblNewLabel_3, gbc_lblNewLabel_3);
+			}
 
-		JLabel lblNewLabel_4 = new JLabel("");
-		lblNewLabel_4.setIcon(new ImageIcon("C:\\Users\\quino\\Downloads\\WhatsApp Image 2022-06-08 at 11. (1).jpg"));
-		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
-		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_4.gridx = 4;
-		gbc_lblNewLabel_4.gridy = 4;
-		panel_4.add(lblNewLabel_4, gbc_lblNewLabel_4);
+		});
 
-		JLabel lblNewLabel_5 = new JLabel("");
-		lblNewLabel_5.setIcon(new ImageIcon("C:\\Users\\quino\\Downloads\\WhatsApp Image 2022-06-08 at 11..jpg"));
-		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
-		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 0);
-		gbc_lblNewLabel_5.gridx = 6;
-		gbc_lblNewLabel_5.gridy = 4;
-		panel_4.add(lblNewLabel_5, gbc_lblNewLabel_5);
+		JLabel letrasJoji = new JLabel("Joji");
+		letrasJoji.setBounds(325, 214, 25, 23);
+		letrasJoji.setFont(new Font("MV Boli", Font.BOLD, 14));
+		letrasJoji.setForeground(Color.WHITE);
+		panel_2.add(letrasJoji);
 
-		JButton moey2 = new JButton("\u25B6");
-		GridBagConstraints gbc_moey2 = new GridBagConstraints();
-		gbc_moey2.insets = new Insets(0, 0, 5, 5);
-		gbc_moey2.gridx = 4;
-		gbc_moey2.gridy = 5;
-		panel_4.add(moey2, gbc_moey2);
+		JButton btnNewButton_1 = new JButton("\u25B6\uFE0E");
+		btnNewButton_1.setBounds(10, 321, 53, 23);
+		panel_2.add(btnNewButton_1);
 
-		JButton moey3 = new JButton("\u25B6");
-		moey3.setForeground(Color.BLACK);
-		GridBagConstraints gbc_moey3 = new GridBagConstraints();
-		gbc_moey3.insets = new Insets(0, 0, 5, 0);
-		gbc_moey3.gridx = 6;
-		gbc_moey3.gridy = 5;
-		panel_4.add(moey3, gbc_moey3);
+		JButton btnNewButton_4 = new JButton("\u25B6\uFE0E");
+		btnNewButton_4.setBounds(240, 321, 53, 23);
+		panel_2.add(btnNewButton_4);
 
-		JLabel lblNewLabel_7 = new JLabel("Joji");
-		lblNewLabel_7.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 12));
+		JButton btnNewButton_4_1 = new JButton("\u25B6\uFE0E");
+		btnNewButton_4_1.setBounds(479, 321, 53, 23);
+		panel_2.add(btnNewButton_4_1);
+
+		JLabel lblNewLabel_7 = new JLabel("");
+		lblNewLabel_7.setBounds(68, 287, 65, 65);
+		lblNewLabel_7.setIcon(new ImageIcon(
+				"D:\\Programacion\\ProyectoFinalSpotify\\ProyectoFinalSpotify\\imagenes\\jojiwillhe.jpg"));
 		lblNewLabel_7.setForeground(Color.WHITE);
-		GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
-		gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_7.gridx = 4;
-		gbc_lblNewLabel_7.gridy = 8;
-		panel_4.add(lblNewLabel_7, gbc_lblNewLabel_7);
+		panel_2.add(lblNewLabel_7);
 
-		JButton btnNewButton_3 = new JButton("\u25B6");
-		GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
-		gbc_btnNewButton_3.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_3.gridx = 1;
-		gbc_btnNewButton_3.gridy = 10;
-		panel_4.add(btnNewButton_3, gbc_btnNewButton_3);
-
-		JLabel lblNewLabel_8 = new JLabel("");
+		JLabel lblNewLabel_8 = new JLabel("\r\n");
+		lblNewLabel_8.setBounds(303, 287, 69, 65);
 		lblNewLabel_8.setIcon(new ImageIcon(
-				"E:\\Programacion\\ProyectoFinalSpotify\\ProyectoFinalSpotify\\imagenes\\jojiwillhe.jpg"));
-		GridBagConstraints gbc_lblNewLabel_8 = new GridBagConstraints();
-		gbc_lblNewLabel_8.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_8.gridx = 2;
-		gbc_lblNewLabel_8.gridy = 11;
-		panel_4.add(lblNewLabel_8, gbc_lblNewLabel_8);
+				"D:\\Programacion\\ProyectoFinalSpotify\\ProyectoFinalSpotify\\imagenes\\jojinectar.jpg"));
+		lblNewLabel_8.setForeground(Color.WHITE);
+		panel_2.add(lblNewLabel_8);
 
-		JLabel lblNewLabel_9 = new JLabel("");
+		JLabel lblNewLabel_9 = new JLabel("\r\n");
+		lblNewLabel_9.setBounds(542, 287, 69, 65);
 		lblNewLabel_9.setIcon(new ImageIcon(
-				"E:\\Programacion\\ProyectoFinalSpotify\\ProyectoFinalSpotify\\imagenes\\jojiballads.jpg"));
-		GridBagConstraints gbc_lblNewLabel_9 = new GridBagConstraints();
-		gbc_lblNewLabel_9.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_9.gridx = 4;
-		gbc_lblNewLabel_9.gridy = 11;
-		panel_4.add(lblNewLabel_9, gbc_lblNewLabel_9);
+				"D:\\Programacion\\ProyectoFinalSpotify\\ProyectoFinalSpotify\\imagenes\\jojiballads.jpg"));
+		lblNewLabel_9.setForeground(Color.WHITE);
+		panel_2.add(lblNewLabel_9);
 
-		JLabel lblNewLabel_10 = new JLabel("");
-		lblNewLabel_10.setIcon(new ImageIcon(
-				"E:\\Programacion\\ProyectoFinalSpotify\\ProyectoFinalSpotify\\imagenes\\jojinectar.jpg"));
-		GridBagConstraints gbc_lblNewLabel_10 = new GridBagConstraints();
-		gbc_lblNewLabel_10.insets = new Insets(0, 0, 5, 0);
-		gbc_lblNewLabel_10.gridx = 6;
-		gbc_lblNewLabel_10.gridy = 11;
-		panel_4.add(lblNewLabel_10, gbc_lblNewLabel_10);
+		JButton btnNewButton_6_3 = new JButton("||");
+		btnNewButton_6_3.setBounds(17, 287, 41, 23);
+		panel_2.add(btnNewButton_6_3);
 
-		JButton btnNewButton = new JButton("");
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton.gridx = 1;
-		gbc_btnNewButton.gridy = 12;
-		panel_4.add(btnNewButton, gbc_btnNewButton);
+		JButton btnNewButton_6_4 = new JButton("||");
+		btnNewButton_6_4.setBounds(252, 287, 41, 23);
+		panel_2.add(btnNewButton_6_4);
 
-		JButton btnNewButton_4 = new JButton("\u25B6");
-		GridBagConstraints gbc_btnNewButton_4 = new GridBagConstraints();
-		gbc_btnNewButton_4.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_4.gridx = 4;
-		gbc_btnNewButton_4.gridy = 13;
-		panel_4.add(btnNewButton_4, gbc_btnNewButton_4);
-
-		JButton btnNewButton_5 = new JButton("\u25B6");
-		btnNewButton_5.setBackground(new Color(240, 240, 240));
-		GridBagConstraints gbc_btnNewButton_5 = new GridBagConstraints();
-		gbc_btnNewButton_5.insets = new Insets(0, 0, 5, 0);
-		gbc_btnNewButton_5.gridx = 6;
-		gbc_btnNewButton_5.gridy = 13;
-		panel_4.add(btnNewButton_5, gbc_btnNewButton_5);
+		JButton btnNewButton_6_5 = new JButton("||");
+		btnNewButton_6_5.setBounds(491, 287, 41, 23);
+		panel_2.add(btnNewButton_6_5);
 		irALibreria.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
